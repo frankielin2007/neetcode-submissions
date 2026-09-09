a615a51fe3e20f7ac80996ba21c66dc6c62cee4a
frankielin2007 <frankielin2007@gmail.com>
2026-09-09 11:01:43 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_profit = prices[0];
+        int max_profit = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices.size() == 1 || prices.size() == 0)
+            {
+                return 0;
+            }
+            int right = i + 1;
+            while (right != prices.size() && prices[right] > prices[i])
+            {
+                int profit = prices[right] - prices[i];
+                max_profit = max(max_profit, profit);
+                right++;
+            }
+        }
+        return max_profit;
+    }
+};
